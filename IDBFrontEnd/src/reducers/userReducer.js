9ad4f58a045726d43fedd53db1b1a29c8c043b32
Sean Eichenberger <sean.eichenberger@noaa.gov>
2018-02-12 11:55:46 -0700

    added seamless-immutable to userReducer - awaiting backend data to test implementation

--- IDBFrontEnd/src/reducers/userReducer.js
+++ IDBFrontEnd/src/reducers/userReducer.js
@@ -1,6 +1,9 @@
+import Immutable from 'seamless-immutable';
+
+//TODO: clean up all commented out return statements once seamless-immutable is verified to work
 
 
-const initialState = {
+const initialState = Immutable({
   fetchingUsers: false,
   fetchedUsers: false,
   error: null,
@@ -16,55 +19,72 @@ const initialState = {
   },
   addingUser: false,
   addedUser: false,
-};
+});
 
 export default function reducer(state = initialState, action) {
   switch (action.type) {
   case 'FETCH_USER': {
-    return { ...state, fetchingUsers: true };
+
+    return Immutable.set(state, "fetchingUsers", true)
+    // return { ...state, fetchingUsers: true };
   }
   case 'FETCH_USER_REJECTED': {
-    return { ...state, fetchingUsers: false, error: action.payload };
+    return Immutable.merge(state, {fetchingUsers: false, error: action.payload})
+    // return { ...state, fetchingUsers: false, error: action.payload };
   }
   case 'FETCH_USER_FULFILLED': {
-    return {
-      ...state,
-      fetchingUsers: false,
-      fetchedUsers: true,
-      users: action.payload,
-    };
+    return Immutable.merge(state, { fetchingUsers: false, fetchedUsers: true, users: action.payload })
+
+    // return {
+    //   ...state,
+    //   fetchingUsers: false,
+    //   fetchedUsers: true,
+    //   users: action.payload,
+    // };
+
   }
   case 'ADD_USER_ATTRIBUTE': {
-    const tmp = { ...state.newUser };
-    tmp[action.payload.key] = action.payload.value;
 
-    return {
-      ...state,
-      newUser: tmp,
-    };
+    return Immutable.setIn(state, ["newUser", action.payload.key], action.payload.value)
+
+    // const tmp = { ...state.newUser };
+    // tmp[action.payload.key] = action.payload.value;
+    //
+    // return {
+    //   ...state,
+    //   newUser: tmp,
+    // };
   }
   case 'UPDATE_USER_GENDER': {
-    const tmp = { ...state.newUser };
-    tmp.gender = action.payload;
 
-    return {
-      ...state,
-      newUser: tmp,
-    };
+    return Immutable.setIn(state, ["newUser", "gender"], action.payload)
+
+    // const tmp = { ...state.newUser };
+    // tmp.gender = action.payload;
+    //
+    // return {
+    //   ...state,
+    //   newUser: tmp,
+    // };
   }
   case 'ADD_USER': {
-    return { ...state, addingUser: true };
+    return Immutable.set(state, "addingUser", true)
+    //return { ...state, addingUser: true };
   }
   case 'ADDED_USER_FULFILLED': {
-    return {
-      ...state,
-      addingUser: false,
-      newUser: initialState.newUser,
-      addedUser: true,
-    };
+    return Immutable.merge(state, { addingUser: false, newUser: initialState.newUser, addedUser: true})
+
+    // return {
+    //   ...state,
+    //   addingUser: false,
+    //   newUser: initialState.newUser,
+    //   addedUser: true,
+    // };
   }
   case 'ADD_USER_REJECTED': {
-    return { ...state, addingUser: false, error: action.payload };
+    return Immutable.merge(state, { addingUser: false, error: action.payload })
+
+    //return { ...state, addingUser: false, error: action.payload };
   }
   }
   return state;
