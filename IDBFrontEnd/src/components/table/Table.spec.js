80d10cda689fed69b95a416d2cd2390c47bd654d
Sean Eichenberger <sean.eichenberger@noaa.gov>
2018-02-23 08:57:00 -0700

    added redux-router and history packages, updated test files for Table and Navbar

--- IDBFrontEnd/src/components/table/Table.spec.js
+++ IDBFrontEnd/src/components/table/Table.spec.js
@@ -2,14 +2,55 @@ import React from 'react';
 import { expect } from 'chai';
 import * as enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16'
+import ReactTable from 'react-table';
 
 import Table from './Table';
 
 enzyme.configure({adapter: new Adapter()})
 
 describe("Navbar component test", ()=>{
-  let wrapper = enzyme.mount(<Table/>)
-  it("should have a data prop", ()=>{
-    expect(wrapper.instance().props().data).to.be.an('array')
-  })
+  let wrapper = enzyme.mount(<Table
+    columns={[{test:'test'}]}
+    data={['test']}
+    defaultPageSize={10}
+    loading={false}
+    title="test"/>
+  );
+
+  it("should include required props as their specified type", ()=>{
+    expect(wrapper.props().data).to.be.an('array');
+    expect(wrapper.props().columns).to.be.an('array');
+    expect(wrapper.props().defaultPageSize).to.be.a('number');
+    expect(wrapper.props().loading).to.be.a('boolean');
+    expect(wrapper.props().title).to.be.a('string');
+  });
+
+  it('should contain a ReactTable component', ()=>{
+    expect(wrapper
+      .find('.TableStyle__container')
+      .childAt(1)
+      .containsMatchingElement(<ReactTable
+        data={['test']}
+        columns={[{test:'test'}]}
+        defaultPageSize={10}
+        expanderDefaults={
+          {
+            sortable: false,
+            resizable: true,
+            filterable: false,
+            width: 35
+          }
+        }
+        //The style height gives fixed header with scroll
+        style={{
+          height: "50%", // This will force the table body to overflow and scroll, since there is not enough room
+          width: "95%"
+        }}
+        className="-striped -highlight"
+        defaultSorted={[{id: 'id', desc: false}]}
+        loading={false}
+      />)
+    ).to.equal(true)
+  });
+
 })
