171edff573062d12e8e6e13744319e865ef6564b
Sean Eichenberger <sean.eichenberger@noaa.gov>
2018-04-23 12:43:08 -0600

    working version with Multivalue Map utilized to generate predicate that is passed to the repository findAll method

--- service/src/main/java/com/idb_backend/mvp/controller/EarthquakeController.java
+++ service/src/main/java/com/idb_backend/mvp/controller/EarthquakeController.java
@@ -2,10 +2,14 @@ package com.idb_backend.mvp.controller;
 
 import com.idb_backend.mvp.domain.model.SignifTsqp;
 import com.idb_backend.mvp.domain.repository.EarthquakeRepository;
+import com.idb_backend.mvp.service.impl.EarthquakeService;
 import com.querydsl.core.types.Predicate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.querydsl.binding.QuerydslPredicate;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class EarthquakeController {
@@ -13,13 +17,17 @@ public class EarthquakeController {
   @Autowired
   EarthquakeRepository earthquakeRepository;
 
+  @Autowired
+  EarthquakeService earthquakeService;
+
 //  @Autowired
 //  ExtendedEarthquakeRepository extendedEarthquakeRepository;
 
   @CrossOrigin(origins = {"http://localhost:8181", "http://localhost:9000"})
   @RequestMapping(value = "/earthquakes", method= RequestMethod.GET)
   @ResponseBody
-  public Iterable<SignifTsqp> getAllEarthquakes(@QuerydslPredicate(root = SignifTsqp.class) Predicate predicate){
-    return earthquakeRepository.findAll(predicate);
+  public Iterable<SignifTsqp> getAllEarthquakes(@RequestParam final MultiValueMap<String, String> map){
+      Predicate predicate = earthquakeService.genPreds(map);
+      return earthquakeRepository.findAll(predicate);
   }
 }
