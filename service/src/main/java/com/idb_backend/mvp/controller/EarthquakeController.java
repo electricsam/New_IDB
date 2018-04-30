e75ff083a1d6e3185c214ad0810d8d512875490d
Sean Eichenberger <sean.eichenberger@noaa.gov>
2018-04-30 10:38:27 -0600

    controller tests for earthquake controller are up

--- service/src/main/java/com/idb_backend/mvp/controller/EarthquakeController.java
+++ service/src/main/java/com/idb_backend/mvp/controller/EarthquakeController.java
@@ -25,26 +25,10 @@ public class EarthquakeController {
   @Autowired
   EarthquakeRepository earthquakeRepository;
 
-  /*
-  * This is a test controller method that will assess the viablility of a custom implementation
-  * */
-  @RequestMapping(value = "/earthquakes/special", method= RequestMethod.GET)
-  @ResponseBody
-  public ResponseEntity getAllEarthquakesAboveNum(){
-    try{
-      Iterable<SignifVsqp> result = earthquakeRepository.findRelatedEarthquake(5200);
-      return ResponseEntity.status(HttpStatus.OK).body(result);
-    }catch (Exception e){
-      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
-    }
-  }
-
   @RequestMapping(value = "/earthquakes", method= RequestMethod.GET)
   @ResponseBody
   public ResponseEntity getAllEarthquakes(@RequestParam Map<String, String> params,
                                           @QuerydslPredicate(root = SignifVsqp.class) Predicate predicate){
-
-//    return ResponseEntity.status(HttpStatus.OK).body(null);
     try{
       if(params.get("tsunamiid") != "" && params.get("tsunamiid") != null){
         Iterable<SignifVsqp> result = earthquakeRepository
@@ -62,7 +46,7 @@ public class EarthquakeController {
     }
   }
 
-//This is will be for immediate removal
+  //This is will be for immediate removal
   @RequestMapping(value = "/earthquakes/tsqp", method= RequestMethod.GET)
   @ResponseBody
   public ResponseEntity getAllEarthquakesTsqp(@QuerydslPredicate(root = SignifTsqp.class) Predicate predicate){
@@ -85,8 +69,6 @@ public class EarthquakeController {
     }
   }
 
-
-  /* NEED TO MOVE PATCH AND POST AND DELETE METHODS TO CALL ANOTHER REPO BECUASE NEED TO RUN AGAINST TSQP AND NOT VSQP*/
   @RequestMapping(value = "/earthquakes/{id}", method= RequestMethod.PATCH)
   @ResponseBody
   public ResponseEntity patchEarthquake(@PathVariable("id") Integer id, @RequestBody SignifTsqp signifTsqp){
