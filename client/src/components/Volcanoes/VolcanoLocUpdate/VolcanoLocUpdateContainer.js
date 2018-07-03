a42aa6b0757db9f0e5099b7505e22f8e778be8fd
Sean Eichenberger <sean.eichenberger@noaa.gov>
2018-07-03 11:09:52 -0600

    refactored validateDateTime in VoclanoEventUpdateContainer and removed it from VolcanoLocUpdateContainer

--- client/src/components/Volcanoes/VolcanoLocUpdate/VolcanoLocUpdateContainer.js
+++ client/src/components/Volcanoes/VolcanoLocUpdate/VolcanoLocUpdateContainer.js
@@ -36,28 +36,6 @@ class VolcanoLocUpdateContainer extends React.Component{
 
   validateMinMax = (val, min, max) => (val >= min && val <= max && !isNaN(val)) || !val ? true : false;
 
-  validateDateTime = (dateTime) => {
-    if (!dateTime) return true
-    var re = /^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}/
-    if(!re.test(dateTime)) return false;
-
-    let date = dateTime.substring(0, 10);
-    let dateArr = date.split('-').map(e => parseInt(e));
-
-    let time = dateTime.substring(11, 19)
-    let timeArr = time.split(':').map(e => parseInt(e));
-
-    if(dateArr[0] > (new Date).getFullYear() || dateArr[0] < 0) return false;
-    if(dateArr[1] > 12 || dateArr[1] < 1) return false;
-    if(dateArr[2] < 1 || dateArr[2] > 31) return false;
-
-    if(timeArr[0] < 0 || timeArr[0] > 23) return false;
-    if(timeArr[1] < 0 || timeArr[1] > 59) return false;
-    if(timeArr[2] < 0 || timeArr[2] > 59) return false;
-
-    return true;
-  };
-
   render(){
     const { volcano } = this.props;
     if(volcano.get("fetchingVolcanoLocs") === true){
@@ -80,7 +58,6 @@ class VolcanoLocUpdateContainer extends React.Component{
                 toggleSection={this.toggleLocation}
                 showSection={volcano.get('showVolLocUpdateLocation')}
                 validateMinMax={this.validateMinMax}
-                validateDateTime={this.validateDateTime}
                 formData={Location}
             />
 
