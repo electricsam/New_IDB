c5747682ef42cfb9118424b8e3cd34a051b80a8f
Sean Eichenberger <sean.eichenberger@noaa.gov>
2018-07-17 16:06:21 -0600

    created ui reducer for volcano and added it to rootReducer, created constants file for definitions

--- client/src/reducers/rootReducer.js
+++ client/src/reducers/rootReducer.js
@@ -3,10 +3,12 @@ import { routerReducer } from 'react-router-redux';
 import rootFormReducer from './rootFormReducer';
 import tsunamiUiReducer from './tsunamiUiReducer';
 import earthquakeUiReducer from './earthquakeUiReducer'
+import volcanoUiReducer from './volcanoUiReducer';
 
 export default combineReducers({
   router: routerReducer,
   deep: rootFormReducer,
   tsunamiUi: tsunamiUiReducer,
-  earthquakeUi: earthquakeUiReducer
+  earthquakeUi: earthquakeUiReducer,
+  volcanoUi: volcanoUiReducer
 });
---
+++ client/src/reducers/volcanoUiReducer.js
@@ -0,0 +1,33 @@
+import {fromJS} from 'immutable';
+
+export const initialState = fromJS({
+  eventModalIsOpen: false,
+  eventModalTitle: null,
+  eventModalValidValues: null,
+  eventModalData: null,
+  eventModalSecondaryData: null,
+  eventModalComponent: null
+});
+
+export default function reducer(state = initialState, action){
+  switch(action.type){
+    case 'OPEN_VOLCANO_EVENT_MODAL': {
+      console.log("action.payload: ", action.payload)
+      return state.merge(state, {
+        eventModalIsOpen: true,
+        eventModalTitle: action.payload.title,
+        eventModalData: action.payload.data,
+        eventModalValidValues: action.payload.validValues,
+        eventModalSecondaryData: action.payload.secondaryData || null,
+        eventModalComponent: action.payload.component || null
+      });
+    }
+    case 'CLOSE_VOLCANO_EVENT_MODAL': {
+      return state.merge(state, {
+        eventModalIsOpen: false
+      });
+    }
+    default:
+      return state;
+  }
+}
