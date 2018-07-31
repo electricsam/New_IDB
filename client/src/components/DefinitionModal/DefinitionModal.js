71d190344a1f5ca6ba178ad3778c8a4e2d4675c9
Sean Eichenberger <sean.eichenberger@noaa.gov>
2018-07-31 14:18:51 -0600

    added autoFocus to the close button of the modal for accessibility

--- client/src/components/DefinitionModal/DefinitionModal.js
+++ client/src/components/DefinitionModal/DefinitionModal.js
@@ -38,7 +38,7 @@ export const DefinitionModal = props => {
 
           <div className={Styles.modal} role="dialog" aria-labelledby="modalTitle" aria-describedby="modalDesc">
             <div className={Styles.modalContent}>
-              <button className={Styles.close} onClick={props.handleCloseModal}>
+              <button autoFocus={true} className={Styles.close} onClick={props.handleCloseModal}>
                 <i className="material-icons">close</i>
               </button>
 
