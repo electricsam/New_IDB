6b676478eaae8a4dd2084ddb8415f94af3cb7fbc
Sean Eichenberger <sean.eichenberger@noaa.gov>
2018-08-16 10:40:36 -0600

    added comments to explain lambda expresison in EarthquakeRepository

--- service/src/main/java/com/idb_backend/mvp/domain/repository/EarthquakeRepository.java
+++ service/src/main/java/com/idb_backend/mvp/domain/repository/EarthquakeRepository.java
@@ -12,6 +12,8 @@ public interface EarthquakeRepository extends JpaRepository<SignifTsqp, Integer>
     QuerydslBinderCustomizer<QSignifTsqp>, EarthquakeCustomRepository {
   @Override
   default void customize(QuerydslBindings bindings, QSignifTsqp root){
+
+    // ensures that all predicates on string values ignore the case of the String value
     bindings.bind(String.class).first((StringPath path, String value) -> path.equalsIgnoreCase(value));
 
   }
